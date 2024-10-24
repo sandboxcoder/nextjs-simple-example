7e8b10f2f2dcf4b66e0c95e6f9cade2110f4e53a
Richard Osborne <rosborne255@yahoo.com>
2024-10-23 20:20:58 -0500

    Adding listItems

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,29 @@ function MyButton() {
   );
 }
 
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
 export default function MyApp() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <ul>{listItems}</ul>
     </div>
   );
 }
