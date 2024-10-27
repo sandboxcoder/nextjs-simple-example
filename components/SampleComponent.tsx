f1ce4b49113f29c9f2560da43dc59cb11cfa4874
Richard Osborne <rosborne255@yahoo.com>
2024-10-26 22:04:41 -0500

    now shows how Constructor can be invoked
    * Adding constructor to the SampleComponent. shows how the constructor can be setup

--- components/SampleComponent.tsx
+++ components/SampleComponent.tsx
@@ -2,7 +2,18 @@
 
 import React, {Component} from 'react';
 
-class SampleComponent extends Component{
+type MyProps = {
+  // using `interface` is also ok
+  message: string;
+};
+type MyState = {
+  count: number; // like this
+};
+class SampleComponent extends Component<MyProps, MyState>{
+  constructor(props: MyProps)
+  {
+    super(props);
+  }
   render() {
     return (
       <div>
