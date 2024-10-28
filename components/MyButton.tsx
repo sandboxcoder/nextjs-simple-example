32e7294c62bd546b07de59bb29475ca6ae1f0bdc
Richard Osborne <rosborne255@yahoo.com>
2024-10-27 20:33:56 -0500

    Updating MyButton example
    * Now prints the number of times it has been clicked

--- components/MyButton.tsx
+++ components/MyButton.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import { useState } from 'react';
+
 export default function MyButton() {
+    const [count, setCount] = useState(0);
     function handleClick() {
-        alert('You clicked me!');
+        setCount(count + 1);
     }
 
     return (
       <button onClick={handleClick}>
-        I'm a button
+        Clicked {count} times
       </button>
     );
   }
